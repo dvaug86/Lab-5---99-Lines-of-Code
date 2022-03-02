ba6553dc00e6b2b9cff6eaa5a2864818cf7b817e
David Vaughan <dvaug86@gmail.com>
2022-03-02 11:53:52 -0500

    first commit for part 2 plus the addition of the working button

--- apps.js
+++ apps.js
@@ -1,18 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+//create button
+var btn = document.createElement('button');
+var btnText = document.createTextNode('Sing!');
+btn.appendChild(btnText);
+document.body.appendChild(btn);
+
+
+
+
+
+
+
 //An array of 5 of my friends
 myFriends = ['JohnAllen', 'Collin', 'Cam', 'Matt', 'Mike'];
 
 
 //newNum = currentNum - 1;
 
-
+btn.addEventListener('click', ()=> {
 for(i=0; i<myFriends.length; i++) {
     console.log(myFriends[i])
    
     for (num = 99; num >= 1; num--){
-        if(num > 1 ){
+        if(num > 2 ){
             console.log(num + " lines of code in the file, " + num + " lines of code; " + myFriends[i] + " clears it all out, " + (num-1) + " lines of code in the file")
+        }if(num===2){
+            console.log(num + " lines of code in the file, " + num + " lines of code; " + myFriends[i] + " clears it all out, 1 more line of code in the file")
         }if (num===1){
         console.log(num + " lines of code in the file, " + num + " lines of code; " + myFriends[i] + " clears it all out, no more lines of code in the file")
         }
     }   
 }
+});
+});
